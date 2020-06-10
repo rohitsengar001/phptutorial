dc13d269435d52e144b1b6302eae632d8ab574d6
ROHIT <rohitsengar001@gmail.com>
2020-06-10 13:22:42 +0530

    DISQUS integrate with basic and default setting

--- blogview.php
+++ blogview.php
@@ -142,10 +142,9 @@ $conn->close();
             <div class="blog-post">
                 <h2 class="blog-post-title">Sample blog post</h2>
                 <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-
-                <p style="font-family: medium-content-serif-font, Georgia, Cambria, " Times New Roman", Times,
-                serif;"> <?php echo $content; ?></p>
-            </div><!-- /.blog-post -->
+                <p style="font-family: medium-content-serif-font, Georgia, Cambria;" Times New Roman", Times, serif;"> <?php echo $content; ?></p>
+            </div>
+            <!-- /.blog-post -->
 
 
             <nav class="blog-pagination">
@@ -154,6 +153,28 @@ $conn->close();
 
             </nav>
             <hr>
+            <!--  CHAT DISCUSSION  FOR BLOG -->
+            <div id="disqus_thread"></div>
+            <script>
+
+                /**
+                 *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                 *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+                /*
+                var disqus_config = function () {
+                this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                };
+                */
+                (function() { // DON'T EDIT BELOW THIS LINE
+                    var d = document, s = d.createElement('script');
+                    s.src = 'https://bloggingyourself.disqus.com/embed.js';
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
         </div><!-- /.blog-main -->
 
         <aside class="col-md-2 blog-sidebar">
@@ -195,6 +216,7 @@ $conn->close();
 
 </main><!-- /.container -->
 <br><br>
+
 
 <?php
 include 'footer.php';
