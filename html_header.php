e3d1ce22394874857cb49b1c5040c8313b8d2687
rohitsengar001 <rohitsengar001@gmail.com>
2021-06-04 15:51:58 +0530

    Revert "postapi successfully implemented apart of exception handling"
    This reverts commit 6b7c226f817c3cd01ac0316e37c0e0cc10430def.

--- html_header.php
+++
@@ -1,6 +0,0 @@
-<?php
-    header('Content-Type:application/json');
-    header('Access-Control-Allow-Method:GET');
-    header('Access-Control-Allow-Origin:*');
-    header('Access-Control-Allow-Headers:Content-Type,Access-Control-Allow-Method,Access-Control-Allow-Origin');
-?>
