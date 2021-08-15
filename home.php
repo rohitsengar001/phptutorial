326989d64fe0c8a0ab2ccf2110d4216f54b7a79a
rohitsengar001 <rohitsengar001@gmail.com>
2021-08-15 08:56:44 +0530

    applied session at home.php

--- home.php
+++ home.php
@@ -1,25 +1,39 @@
 <!---@include files-->
 <?php
+session_start();
 include 'header.php';
-include 'content.php';
 include 'footer.php';
+// try{
+if (!empty($_SESSION["name"])) {
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Home</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-</head>
-<body>
+    </head>
 
-</body>
+    <body>
+        <?php
+        
+        include 'content.php';
+        ?>
 
-</html>
-<script type="text/javascript">
+    </body>
 
-    // Initialize CKEditor
+    </html>
+    <script type="text/javascript">
+        // Initialize CKEditor
+    </script>
+<?php
+} else{
+        echo "kindly login first";
+        header("refresh:5;url=login.php");
 
-
-</script>
+}
+// }catch(Exception $e){
+// }
+?>
